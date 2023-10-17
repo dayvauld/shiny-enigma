80ef8c6335bdb011c5b73839dc6eb7de96ee50b8
David Auld <david.a.auld@gmail.com>
2023-10-17 09:43:45 -0700

    init

---
+++ Package.swift
@@ -0,0 +1,54 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "SwiftWhisperStream",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15),
+        .tvOS(.v13),
+    ],
+    products: [
+        .library(name: "SwiftWhisperStream", targets: ["SwiftWhisperStream"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ctreffs/SwiftSDL2", branch: "master"),
+    ],
+    targets: [
+        .target(name: "SwiftWhisperStream", dependencies: [.target(name: "whisper_cpp"), .target(name: "LibWhisper")]),
+        .target(name: "LibWhisper", dependencies: [
+            .target(name: "whisper_cpp"),
+        ]),
+        .target(
+            name: "whisper_cpp",
+            dependencies: [
+                .product(name: "SDL", package: "SwiftSDL2"),
+            ], 
+            exclude: [
+                "ggml-metal.metal",
+            ],
+            resources: [
+                .process("ggml-metal.metal"),
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .unsafeFlags(["-Wno-shorten-64-to-32"]),
+                .define("GGML_USE_ACCELERATE", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                .define("GGML_USE_METAL", .when(platforms: [.macOS, .macCatalyst, .iOS])),
+                .unsafeFlags(["-O3"]),
+                .unsafeFlags(["-DNDEBUG"]),
+                .unsafeFlags(["-pthread"]),
+                .unsafeFlags(["-fno-objc-arc"]),
+            ]//,
+//            linkerSettings: [
+//                .linkedFramework("Foundation"),
+//                .linkedFramework("Accelerate"),
+//                .linkedFramework("Metal"),
+//                .linkedFramework("MetalKit"),
+//                .linkedFramework("MetalPerformanceShaders"),
+//            ]),
+            ),
+//        .testTarget(name: "WhisperTests", dependencies: [.target(name: "SwiftWhisper")], resources: [.copy("TestResources/")])
+    ],
+    cxxLanguageStandard: CXXLanguageStandard.cxx20
+)
